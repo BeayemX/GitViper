92447df76132132c42fe986ebdab48b06c8b4068
Christian Winter <christian.winter1990@gmail.com>
2018-11-09 23:30:13 +0100

    Fix multiple task-occurences not showing in TaskList. Resolves #6

--- TaskList.py
+++ TaskList.py
@@ -35,6 +35,77 @@ def fill_dictionary(task, file):
             occ = Occurence(file[0], file[1], o[0], o[1])
             occurences_dict[task].append(occ)
 
+
+def split_stream(occurence, task): # TODO rename
+    stream = occurence.linecontent
+    key = task.value
+
+    last_index = 0
+    all_lines = []
+
+    stream = stream.strip()
+    # strip commenting symbols
+    if settings.strip_comment_symbols:
+        stream = stream.replace("#", "")
+        stream = stream.replace("//", "")
+        stream = stream.replace("/*", "")
+        stream = stream.replace("\t", " ")
+        stream = stream.strip()
+
+
+    stream_split = stream.lower().split(key.lower())
+
+    for s in stream_split:
+        real_value_entry = []
+
+        final_string = ""
+        start_index = stream.lower().find(s, last_index)
+        s_length = len(s)
+
+        task_start_idx = start_index - len(key) # the task keywords is before 's'
+        task_end_idx = start_index
+
+        # line ends with task keyword?
+        if start_index + len(key) == len(stream):
+            #raise Exception(BOLD + RED + "when does this happen?" + RESET)
+            task_end_idx = len(stream)
+            task_start_idx = task_end_idx - len(key)
+            start_index = task_end_idx
+
+        last_index = start_index + s_length # has to be before 'continue'
+
+        # every logic part should be before this, only printing afterwards
+        if task_start_idx < 0:
+            continue
+
+        orig_task = BOLD + task.color + stream[task_start_idx : task_end_idx] + RESET
+
+        before = stream[ 0 : start_index - len(key)]
+        after = stream[start_index : ]
+
+        # add to data used for creating table
+        if settings.show_paths_for_task_list == args.toggle_paths:
+            path = occurence.path.replace(os.getcwd(), "") + "/"
+            path = path.lstrip("/")
+        else:
+            path = ""
+
+        real_value_entry.append(window_padding + task.representation) # TODO remove, not used any more
+        real_value_entry.append(before + orig_task + after)
+
+        if settings.show_paths_for_task_list == args.toggle_paths:
+            real_value_entry.append(path + BOLD + task.color + occurence.filename.rjust(0).strip() + RESET)
+        else:
+            real_value_entry.append(path + occurence.filename.rjust(0).strip())
+
+        real_value_entry.append(str(occurence.linenumber))
+
+
+        all_lines.append(real_value_entry)
+
+    return all_lines
+
+
 def list_tasks(cli_tasks):
     # TODO should avoid adding unused Tasks in the first place instead of adding them and then clearing the list
     if args.ignore_conf:
@@ -45,6 +116,7 @@ def list_tasks(cli_tasks):
         for cli_task in cli_tasks:
             settings.add_task(Task(cli_task, None, None, None, None, None))
 
+    # initialize dict
     for k in settings.tasks:
         occurences_dict[k] = []
 
@@ -60,6 +132,8 @@ def list_tasks(cli_tasks):
     occurences_dict_list = []
     for key in occurences_dict:
         if len(occurences_dict[key]) == 0:
+            # TODO use some sort of Debug print to be able to show when using --debug
+            # print("No tasks for: " + key.representation)
             continue
         else:
             occurences_dict_list.append((key, occurences_dict[key]))
@@ -71,52 +145,13 @@ def list_tasks(cli_tasks):
 
     # print table
     for kv in occurences_dict_sorted_by_priority:
-        key = kv[0]
-        value = kv[1]
+        task = kv[0] # Task
+        occurence_list = kv[1] # Occurence []
         real_values = []
 
-        for o in value:
-            real_value_entry = []
-            linecontent = o.linecontent.strip()
-
-            # strip task regardless of capitalization
-            idx = linecontent.lower().find(key.value)
-            orig_keyword = linecontent[ idx : idx + len(key.value) ]
-            splits = linecontent.split(linecontent[idx:idx+len(key.value)])
-
-            linecontent = ""
-            if idx != 0:
-                linecontent += splits[0]
-
-            linecontent += key.color + orig_keyword + RESET
-            linecontent += splits[-1] # splits[1] would have to be checked if idx == 0
-
-            # strip commenting symbols
-            if settings.strip_comment_symbols:
-                linecontent = linecontent.replace("#", "")
-                linecontent = linecontent.replace("//", "")
-                linecontent = linecontent.replace("/*", "")
-                linecontent = linecontent.replace("\t", " ")
-                linecontent = linecontent.strip()
-
-            # add to data used for creating table
-            if settings.show_paths_for_task_list == args.toggle_paths:
-                path = o.path.replace(os.getcwd(), "") + "/"
-                path = path.lstrip("/")
-            else:
-                path = ""
-
-            real_value_entry.append(window_padding + key.representation)
-            real_value_entry.append(linecontent)
-
-            if settings.show_paths_for_task_list == args.toggle_paths:
-                real_value_entry.append(path + BOLD + key.color + o.filename.rjust(0).strip() + RESET)
-            else:
-                real_value_entry.append(path + o.filename.rjust(0).strip())
-
-            real_value_entry.append(str(o.linenumber))
-
-            real_values.append(real_value_entry)
+        for o in occurence_list:
+            for line in split_stream(o, task):
+                real_values.append(line)
 
         # find max column widths
         substitutes = [0] * 4 # substitutes for invisible characters
@@ -141,17 +176,17 @@ def list_tasks(cli_tasks):
 
         # clamp to window width
         s = len(spacing)
-        availablespace = int(utils.get_window_size().x) - max_widths[0] - s - max_widths[2] - s - max_widths[3] - s - len(window_padding) + substitutes[1] + s + 2 # the last ' + s + 2' is for setting [0] to 'line_beginning_spacing' and add whitespace to the right side
+        availablespace = int(utils.get_window_size().x) - max_widths[0] - s - max_widths[2] - s - max_widths[3] - s - len(window_padding) + substitutes[1] + s + 6 # the last ' + s + 2' is for setting [0] to 'line_beginning_spacing' and add whitespace to the right side
         max_widths[1] = availablespace
 
-        print(key.color + BOLD + v[0] + " [" + str(len(real_values)) + "]" + RESET)
+        print(" " + task.color + BOLD + task.representation + " [" + str(len(real_values)) + "]" + RESET)
         print()
 
         for v in real_values:
             if len(v[1]) > max_widths[1]:
                 v[1] = v[1][:max_widths[1]-3] + "..."
             # TODO remove v[0] and max_width[0] because they are not used any more
-            # taskword = key.color + v[0].ljust(max_widths[0]) + spacing + RESET # legacy
+            # taskword = task.color + v[0].ljust(max_widths[0]) + spacing + RESET # legacy
             taskword = line_beginning_spacing
             line = v[1][:max_widths[1]].ljust(max_widths[1]) + spacing
             filename = v[2].ljust(max_widths[2] + substitutes[2]) + spacing
