a279c2f9bd5f4b3abbdff5d32ba5dc4fd5774bc9
Christian Winter <christian.winter1990@gmail.com>
2018-11-10 02:12:50 +0100

    Add possibility to specifiy row for Task

--- gitviper/task.py
+++ gitviper/task.py
@@ -2,10 +2,11 @@ from gitviper.colors import string_to_color
 class Task:
     # default parameters do not work because these values
     # will be parsed from config file and 'None' will be forwarded
-    def __init__(self, representation, value, color, priority, font_color, bold):
+    def __init__(self, representation, value, color, priority, font_color, bold, row):
         self.representation = representation
         self.value = value if value != None else representation.lower()
         self.priority = priority if priority != None else 0 # priorities below 1 will be displayed in the second line
+        self.row = row if row != None else 0
 
         # prepare local variables for string to color-class conversion
         color = color if color != None else "white"
