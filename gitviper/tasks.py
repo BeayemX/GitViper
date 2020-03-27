580d38db7f475e1eb435428b78c91fe9cc4ad1c1
Christian Winter <christian.winter1990@gmail.com>
2020-03-27 23:50:43 +0100

    Improve Task display for current diff

--- gitviper/tasks.py
+++ gitviper/tasks.py
@@ -12,37 +12,74 @@ taskspacing = " " * 2
 initial_spacing = " " * 2
 
 # Configuration
-DISPLAY_RESOLVED_TASKS = True
+
+# TODO Put these settings into a config file and/or make available through CLI
+conf = {
+	'list_occurences': True,
+	'show_staged': True,
+	'show_unstaged': True,
+	'show_resolved': True
+}
 
 def list_tasks_of_diff(list_occurences):
+	conf['list_occurences'] = list_occurences
 	try:
-		_list_tasks_of_diff(list_occurences)
+		_list_tasks_of_diff()
 	except ValueError as e:
 		print("Could not list tasks of diff!")
 		print(e)
 
-def _list_tasks_of_diff(list_occurences):
-	t = connection.repo.head.commit.tree
-	x = connection.repo.git.diff(t, '--color=always').split('\n')
-	y = connection.repo.git.diff(t, '--color=always', '--staged').split('\n')
+def _list_tasks_of_diff():
+	t = connection.repo.head.commit
+	if conf['show_staged']:
+		staged_diff = connection.repo.git.diff(t, '--color=always', '--staged').split('\n')
+	if conf['show_unstaged']:
+		unstaged_diff = connection.repo.git.diff(t, '--color=always').split('\n') # FIXME this includes staged changes. Maybe compare against sth else than 'head.commit'
 
-	z = y # list only for staged
-	z = x # list staged and unstaged
+	# Remove staged lines from unstaged diff
+	if conf['show_staged'] and conf['show_unstaged']:
+		for diff_line in staged_diff:
+			for unstaged_diff_line in unstaged_diff:
+				if diff_line == unstaged_diff_line:
+					unstaged_diff.remove(diff_line)
 
-	final_lines = []
-	additions = []
-	deletions = []
+	def _sanitize(lines):
+		additions = []
+		deletions = []
 
-	for line in z:
-		if line.startswith(GREEN):
-			new_line = line.lstrip(GREEN)[1:]
-			final_lines.append(new_line)
-			additions.append(new_line)
-		elif line.startswith(RED):
-			new_line = line.lstrip(RED)[1:]
-			final_lines.append(new_line)
-			deletions.append(new_line)
+		for line in lines:
+			if line.startswith(GREEN):
+				new_line = line.lstrip(GREEN)[1:]
+				additions.append(new_line)
+			elif line.startswith(RED):
+				new_line = line.lstrip(RED)[1:]
+				deletions.append(new_line)
 
+		return additions, deletions
+
+	if conf['show_staged']:
+		staged_additions, staged_deletions = _sanitize(staged_diff)
+	if conf['show_unstaged']:
+		unstaged_additions, unstaged_deletions = _sanitize(unstaged_diff)
+
+	has_content = False
+
+	# List staged Tasks
+	if conf['show_staged']:
+		has_content = create_stage_area("Staged", f"{BG_RED}{BOLD}{WHITE}", staged_additions, staged_deletions, f"{BOLD}{RED}", f"{BOLD}{RED}")
+
+	if has_content:
+		print()
+
+	# List unstaged Tasks
+	if conf['show_unstaged']:
+		has_content |= create_stage_area("Unstaged", f"{BG_YELLOW}{BOLD}{BLACK}", unstaged_additions, unstaged_deletions, f"{BOLD}{YELLOW}", f"{BOLD}{YELLOW}")
+
+	# Finalize Tasks area
+	if has_content:
+		draw_separator()
+
+def create_stage_area(title, title_color, additions, deletions, add_color, del_color):
 	added_counter_dict = {}
 	deleted_counter_dict = {}
 
@@ -58,7 +95,8 @@ def _list_tasks_of_diff(list_occurences):
 			if task.value in line:
 				added_counter_dict[task.value] += 1
 				added_not_empty = True
-		if DISPLAY_RESOLVED_TASKS:
+
+		if conf['show_resolved']:
 			for line in deletions:
 				if task.value in line:
 					deleted_counter_dict[task.value] += 1
@@ -66,6 +104,7 @@ def _list_tasks_of_diff(list_occurences):
 
 
 	def list_tasks_in_changes(lines):
+		win_x = utils.get_window_size().x
 		for task in task_loader.tasks:
 			# occurence_happened = False
 			for line in lines:
@@ -73,60 +112,50 @@ def _list_tasks_of_diff(list_occurences):
 					# occurence_happened = True
 					l = remove_color_codes_from_string(line).strip()
 					line_parts = l.split(task.value, 1)
-					l = f"  {line_parts[0]}{BOLD}{task.color}{task.value}{RESET}{line_parts[1]}"
+					l = f"   {line_parts[0]}{BOLD}{task.color}{task.value}{RESET}{line_parts[1]}"
+					if len(remove_color_codes_from_string(l)) > win_x:
+						l = l[:win_x + 3] + "..."
+						remove_color_codes_from_string
+
 					print(f"{RESET}{WHITE}{l}")
 			# if occurence_happened:
 				# print()
 
 
+	def _print_task_area(title, color, dictionary, changes, use_full_line=False):
+		displayed_title = f' {title}'
+		if use_full_line:
+			line = displayed_title + ' ' * (utils.get_window_size().x - len(displayed_title))
+		else:
+			line = displayed_title + '  '
+
+		print(f"{color}{line}{RESET}")
+
+		# print_tasks(dictionary) # Show badges
+
+		if conf['list_occurences']:
+			list_tasks_in_changes(changes)
+
+	# todo
+	has_content = added_not_empty or (conf['show_resolved'] and deleted_not_empty)
+	if has_content:
+		print(f"{title_color}  {title}  {RESET}")
+
 	if added_not_empty:
-		# Highlight line
-		title =  '  Newly introduced tasks'
-		line = title + ' ' * (utils.get_window_size().x - len(title))
-		print(f"{BG_RED}{BOLD}{line}{RESET}")
-		print()
+		_print_task_area("[ Added ]", add_color, added_counter_dict, additions)
 
-		# Print actual Tasks
-		print_tasks(added_counter_dict)
-
-		if list_occurences:
-			list_tasks_in_changes(additions)
-
-		#title =  ''
-		#line = title + ' ' * (utils.get_window_size().x - len(title))
-		#print(f"{BG_RED}{line}{RESET}")
-
-		# Highlight line
-		#print()
-
-	if DISPLAY_RESOLVED_TASKS and deleted_not_empty:
-		# Highlight line
-		title =  '  Resolved tasks'
-		line = title + ' ' * (utils.get_window_size().x - len(title))
+	if conf['show_resolved'] and deleted_not_empty:
 		if added_not_empty:
 			print()
 
-		print(f"{BG_GREEN}{BLACK}{line}{RESET}")
-		print()
+		_print_task_area("[ Resolved ]", del_color, deleted_counter_dict, deletions)
 
-		# Print actual Tasks
-		print_tasks(deleted_counter_dict)
+	return has_content
 
-		if list_occurences:
-			list_tasks_in_changes(deletions)
-
-
-		#title =  ''
-		#line = title + ' ' * (utils.get_window_size().x - len(title))
-		#print(f"{BG_GREEN}{line}{RESET}")
-
-		# Highlight line
-		#print()
-
-	if added_not_empty or (DISPLAY_RESOLVED_TASKS and deleted_not_empty):
-		line = '_' * utils.get_window_size().x
-		print(f"{WHITE}{line}{RESET}")
-		print()
+def draw_separator():
+	line = '_' * utils.get_window_size().x
+	print(f"{WHITE}{line}{RESET}")
+	print()
 
 def list_tasks():
 	counter_dict = {}
