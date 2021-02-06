bd5af0ae8ea4676ea3ef9d9376582ebb52461caf
Christian Winter <christian.winter1990@gmail.com>
2021-02-06 21:41:08 +0100

    Add autocomplete file for bash

---
+++ autocomplete.bash
@@ -0,0 +1,24 @@
+#/usr/bin/env bash
+
+function _get_gitviper_commands() {
+    echo status
+    echo log
+    echo tasks # FIXME 'gitviper tasks' does not work
+    echo stash
+    echo branch
+}
+
+_gitviper_command_list()
+{
+    # Avoid auto completing multiple values
+    if [ "${#COMP_WORDS[@]}" == "2" ]; then
+        COMPREPLY=($(compgen -W "$(_get_gitviper_commands)" -- "${COMP_WORDS[1]}"))
+    #elif [ "${#COMP_WORDS[@]}" == "3" ]; then
+        #COMPREPLY=($(compgen -W "$(_get_gitviper_commands)" -- "${COMP_WORDS[1]}"))
+        #return
+    fi
+    return
+}
+
+complete -F _gitviper_command_list gitviper
+complete -F _gitviper_command_list gv
